fcc5415f1821cbd16929d9c89b92fa3783178ca9
zer0Black <lxtalx@gmail.com>
2018-02-18 00:36:32 +0800

    【添加】webConfig控制注册过滤器

---
+++ jero-core/src/main/java/com/lxt/core/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.lxt.core.config;
+
+import com.lxt.core.filter.CorsFilter;
+import com.lxt.core.filter.DecodeUrlFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Web配置，用于注入过滤器，拦截器等
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new CorsFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("corsFilter");
+        registration.setOrder(10);
+        return registration;
+    }
+
+    @Bean
+    public FilterRegistrationBean decodeFilter() {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(new DecodeUrlFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("decodeFilter");
+        registration.setOrder(11);
+        return registration;
+    }
+
+}
